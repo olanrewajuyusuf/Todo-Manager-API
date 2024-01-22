ebd73d08f8deff455c82293721d466b03b900103
Wallewdev <wallewdev@gmail.com>
2024-01-22 13:35:05 +0100

    Updated

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const tasks = require('./routes/task')
+const notFound = require('./middleware/notFound')
+const errorHandlerMiddleware = require('./middleware/error-handler')
 const connectDB = require('./db/connect')
 require('dotenv').config()
 
@@ -10,10 +12,13 @@ app.use(cors());
 app.use(express.json());
 
 // routes
-app.use('/api/v1/tasks', tasks)
+app.use('/api/v1/tasks', tasks);
+
+app.use(notFound);
+app.use(errorHandlerMiddleware);
 
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 const start = async () => {
     try {
@@ -25,3 +30,16 @@ const start = async () => {
 }
 
 start();
+
+// var twoSum = function(nums, target) {
+//     for (let i = 0; i < nums.length; i++){
+//         for (let j = i + 1; j < nums.length; j++){
+//             if (nums[j] === target - nums[i] ) {
+//                return [i, j]
+//             } 
+//         }
+//     }
+//     return null;
+// };
+// const n = twoSum([3,3], 6);
+// console.log(n);
